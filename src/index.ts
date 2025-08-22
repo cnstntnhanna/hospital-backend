a2f4b1dffd081596c4a33f4dc7acf4f3d87d0686
Tom <tomervingmz@gmail.com>
2025-08-22 17:50:56 +0800

    Fix import path for routes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import routes from "./routes";
+import routes from './routes/index';
+
 
 dotenv.config();
 const app = express();
