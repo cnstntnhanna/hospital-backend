8736083a2240a74bd5c18b80a44a13ae0a155608
Tom <tomervingmz@gmail.com>
2025-08-22 21:03:43 +0800

    -changed origin

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import routes from './routes/index.js';
 
-
-dotenv.config();
 const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({
+  origin: process.env.FRONTEND_URL, // your frontend domain
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
-app.use("/api", routes);
+// routes
+app.get("/", (req, res) => {
+  res.send("Backend running with CORS enabled 🚀");
+});
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`BFF running on port ${PORT}`));
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running...");
+});
